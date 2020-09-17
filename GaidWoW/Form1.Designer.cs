570cd7c02c938f09ab557bb0297134e13f11bc9d
civicml <59001806+civicml@users.noreply.github.com>
2020-09-18 02:39:45 +0300

    Добавьте файлы проекта.

---
+++ GaidWoW/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace GaidWoW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Клик :)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Воин",
+            "Друид",
+            "Жрец",
+            "Маг",
+            "Охотник",
+            "Паладин",
+            "Разбойник",
+            "Рыцарь смерти",
+            "Чернокнижник",
+            "Шаман"});
+            this.comboBox1.Location = new System.Drawing.Point(22, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(120, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберите класс";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(148, 39);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(126, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Выберите специализацию";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(179, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "developer by Чвипка";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 150);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WoWGaid(4.3.4)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GaidWoW/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GaidWoW
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case ("Друид"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Баланс"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-balans-druidu-4-3");
+                            break;
+                        case ("Сила зверя"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-feral-druidu-4-3");
+                            break;
+                        case ("Исцеление"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-restor-druidu-4-3");
+                            break;
+                        case ("Страж"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-druidu-tanku-4-3");
+                            break;
+                    }
+                    break;
+                case ("Воин"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Оружие"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-arms-varu-4-3");
+                            break;
+                        case ("Неистовство"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-furi-varu-4-3");
+                            break;
+                        case ("Защита"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-varu-tanku-4-3");
+                            break;
+                    }
+                    break;
+                case ("Жрец"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Послушание"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-dc-pristu-4-3");
+                            break;
+                        case ("Свет"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-holi-pristu-4-3");
+                            break;
+                        case ("Тьма"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-shp-pristu-4-3");
+                            break;
+                    }
+                    break;
+                case ("Чернокнижник"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Колдовство"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-afli-loku-4-3");
+                            break;
+                        case ("Демонология"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-loku-demonologu-4-3");
+                            break;
+                        case ("Разрушение"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-destro-loku-4-3");
+                            break;
+                    }
+                    break;
+                case ("Разбойник"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Ликвидация"):
+                            System.Diagnostics.Process.Start("https://www.noob-club.ru/index.php?topic=15460.0");
+                            break;
+                        case ("Головорез"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-combat-roge-4-3");
+                            break;
+                        case ("Скрытность"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-shd-roge-4-3");
+                            break;
+                    }
+                    break;
+                case ("Шаман"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Стихии"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-elem-shamanu-4-3");
+                            break;
+                        case ("Совершенствование"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-enh-shamanu-4-3");
+                            break;
+                        case ("Исцеление"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-restor-shamanu-4-3");
+                            break;
+                    }
+                    break;
+                case ("Паладин"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Свет"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-holi-paladinu-4-3");
+                            break;
+                        case ("Защита"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-paladinu-tanku-4-3");
+                            break;
+                        case ("Воздаяние"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-retri-paladinu-4-3-wow");
+                            break;
+                    }
+                    break;
+                case ("Охотник"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Повелитель зверей"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-bm-xantu-4-3");
+                            break;
+                        case ("Стрельба"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-mm-hantu-4-3");
+                            break;
+                        case ("Выживание"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-surv-hantu-4-3");
+                            break;
+                    }
+                    break;
+                case ("Рыцарь смерти"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Кровь"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-dk-tanku-4-3");
+                            break;
+                        case ("Лед"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-frost-dk-4-3");
+                            break;
+                        case ("Нечестивость"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-anholi-dk-4-3");
+                            break;
+                    }
+                    break;
+                case ("Маг"):
+                    switch (comboBox2.Text)
+                    {
+                        case ("Лед"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-frost-magu-4-3");
+                            break;
+                        case ("Огонь"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-faer-magu-4-3");
+                            break;
+                        case ("Тайная магия"):
+                            System.Diagnostics.Process.Start("http://wowgaid.ru/class/gajd-po-arkan-magu-4-3");
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case ("Друид"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Баланс", "Сила зверя", "Исцеление", "Страж" });
+                    break;
+                case ("Воин"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Оружие", "Неистовство", "Защита" });
+                    break;
+                case ("Жрец"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Послушание", "Свет", "Тьма" });
+                    break;
+                case ("Чернокнижник"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Колдовство", "Демонология", "Разрушение" });
+                    break;
+                case ("Разбойник"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Ликвидация", "Головорез", "Скрытность" });
+                    break;
+                case ("Шаман"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Стихии", "Совершенствование", "Исцеление" });
+                    break;
+                case ("Паладин"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Свет", "Защита", "Воздаяние" });
+                    break;
+                case ("Охотник"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Повелитель зверей", "Стрельба", "Выживание" });
+                    break;
+                case ("Рыцарь смерти"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Кровь", "Лед", "Нечестивость" });
+                    break;
+                case ("Маг"):
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.AddRange(new string[] { "Лед", "Огонь", "Тайная магия" });
+                    break;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ничего не скажу!");
+        }
+    }
+}
